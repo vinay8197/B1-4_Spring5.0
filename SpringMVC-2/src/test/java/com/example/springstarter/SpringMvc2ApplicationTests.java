501feac731796a92e39cdd6c1c1525c7d23fe8b6
Vinay <Vinay@LAPTOP-RA2PJ2KK>
2022-06-14 21:21:41 +0530

    first commit

---
+++ SpringMVC-2/src/test/java/com/example/springstarter/SpringMvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
